9dad999e14f668112eb09761cf69aaa5b38fd740
Calleb Camargo <camargocalleb12@gmail.com>
2024-08-05 21:25:39 -0300

    update02-05-08-2024

---
+++ exemplos-faculdade-descomplica/src/desenvolvimento-dinamico02/programacao-com-javaScript-Conceitos02/main.js
@@ -0,0 +1,21 @@
+console.log("testando");
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoa = "Lucas";
+var NOMEPESSO = "Lucas";
+console.log(nomePessoa);
+console.log(NOMEPESSO);
+
+var var1 = 1,
+  var2 = 2,
+  var3 = 3;
+console.log(var2);
+
+let book = 10;
+console.log(book);
+
+const valor1 = 100; //não pode alterar valor
+console.log(valor1);
