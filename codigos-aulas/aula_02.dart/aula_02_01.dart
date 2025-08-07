cf2b7657377fc11244e43a3ff3eed4a4300fcee0
Calleb Camargo <113070078+Calleb21@users.noreply.github.com>
2025-08-07 20:48:11 -0300

    codigos-aula-desenvolvimento-mobile
    Aula de inicialização e apresentação das tecnologias Flutter e Dart no qual foi apresentado conceitos básicos da linguagem.

---
+++ codigos-aulas/aula_02.dart/aula_02_01.dart
@@ -0,0 +1,14 @@
+void main() {
+  // print("Hello, World!");
+
+  String name = "João";
+
+  int age = 30;
+
+  double height = 1.75;
+
+  bool isStudent = false;
+
+  print("Name: $name, Age: $age, Height: $height, Is Student: $isStudent");
+}
+
---
+++ codigos-aulas/aula_02.dart/aula_02_02.dart
@@ -0,0 +1,39 @@
+/*
+void sayHello() {
+  print("Hello! Seja bem-vindo ao Dart!");
+}
+
+void main() {
+  sayHello();
+}
+*/
+
+/* void greet(String name) {
+  print("Hello, $name! Seja bem-vindo ao Dart!");
+}
+
+void main() {
+  greet("João");
+}
+*/
+
+/* int add(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  int result = add(5, 3);
+  print("The sum is: $result");
+}
+*/
+
+/* void main() {
+
+  var list = ['apple', 'banana', 'cherry'];
+
+  list.forEach((item) {
+    print(item);
+  });
+}
+
+*/
